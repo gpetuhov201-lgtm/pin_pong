f807096a03d25d21c11a03c9fa2acc90ebbe304d
gpetuhov201-lgtm <gpetuhov201@gmail.com>
2026-03-09 21:15:29 +0300

    add class ball add ball moving

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from pygame import *
 from models import *
 from random import *
-from time import time as t
+from time import time as t,sleep
 
 window = display.set_mode((700,500))
 
-wall1 = Wall(0,60,'media/images/rocket.png',30,150,2,window)
-wall2 = Wall(670,60,'media/images/rocket.png',30,150,2,window)
+wall1 = Wall(0,150,'media/images/rocket.png',30,150,2,window)
+wall2 = Wall(670,150,'media/images/rocket.png',30,150,2,window)
+ball = Ball(50,250,'media/images/ball.png',40,40,0,window)
 
 
 flag = True
 
 FPS = 60
 clock = time.Clock()
+sleep_flag = False
 while flag:
     window.fill((31, 171, 184))
+    sleep_flag = ball.add_point(wall1,wall2)
     wall1.draw()
     wall2.draw()
     wall1.moving_wasd()
     wall2.moving_arrow()
+    ball.draw()
+    ball.ball_moving()
+    ball.ball_touch(wall1,wall2)
     display.update()
+    if sleep_flag:
+        sleep(1)
     clock.tick(FPS)
     events = event.get()
     for i in events:
--- models.py
+++ models.py
@@ -26,3 +26,34 @@ class Wall(Character):
             self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y <= self.window.get_size()[1] - self.rect.h:
             self.rect.y += self.speed
+
+
+class Ball(Character):
+    speed_x = 3
+    speed_y = 3
+    win_left_counter = 0
+    win_right_counter = 0
+    def ball_moving(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+    def ball_touch(self,wall1,wall2):
+        if self.rect.y <= 0 or self.rect.y >= self.window.get_size()[1] - self.rect.h:
+            self.speed_y *= -1
+        if self.rect.colliderect(wall1.rect) or self.rect.colliderect(wall2.rect):
+            self.speed_x *= -1
+    def add_point(self,wall1,wall2):
+        if self.rect.x <= 0:
+            self.win_left_counter += self.start_point(wall1,wall2)
+            return True
+        if self.rect.x >= self.window.get_size()[0] - self.rect.w:
+            self.win_right_counter += self.start_point(wall1,wall2)
+            return True
+    def start_point(self,wall1,wall2):
+        self.rect.x = self.window.get_size()[0]/2 - self.rect.w
+        self.rect.y = self.window.get_size()[1]/2 - self.rect.h
+        self.speed_x = 3
+        self.speed_y = 3
+        wall1.rect.y = self.window.get_size()[1]/2 - self.rect.h
+        wall2.rect.y = self.window.get_size()[1]/2 - self.rect.h
+
+        return 1
